597aaf167e08204a0b8e885722615cf14048f64e
miroswd <altamirsantos93@hotmail.com>
2019-11-28 20:21:56 -0300

    Loader Users

--- src/routes.js
+++ src/routes.js
@@ -1,8 +1,18 @@
 import { Router } from 'express';
 
+// Importando Models
+import User from './app/models/User';
+
 // Pegando apenas o router de express
 const routes = new Router();
 
-routes.get('/teste', (req, res) => res.json({ msg: 'Hello World!' }));
+routes.get('/', async (req, res) => {
+  const user = await User.create({
+    name: 'Miro',
+    email: 'miroswd@email.com',
+    password_hash: 'senha1234',
+  });
+  return res.json(user);
+});
 
 export default routes; // Será importado em app.js como global
