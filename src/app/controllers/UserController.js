5bfe0145362579ff625503e6a2d1578e2219a9bf
miroswd <altamirsantos93@hotmail.com>
2019-11-30 01:19:07 -0300

    Validation

--- src/app/controllers/UserController.js
+++ src/app/controllers/UserController.js
@@ -1,9 +1,29 @@
 // Feature de criação
+import * as Yup from 'yup'; // importando tudo como yup
 import User from '../models/User';
+
+// Biblioteca de validação de dados
 
 class UserController {
   async store(req, res) {
-    // mesma face de um middleware
+    const schema = Yup.object().shape({
+      name: Yup.string().required(),
+      email: Yup.string()
+        .required()
+        .email(),
+      password: Yup.string()
+        .required()
+        .min(6),
+    });
+
+    // req.body == objeto
+    // formato do dado = shape
+
+    // Verificando se o req.body está passando conforme o schema
+    if (!(await schema.isValid(req.body))) {
+      // retorna false
+      return res.status(400).json({ error: 'Validation fails' });
+    }
 
     // Validando o email
     const userExists = await User.findOne({ where: { email: req.body.email } });
@@ -24,6 +44,24 @@ class UserController {
 
   // Rota de atualização, é necessário estar autenticado
   async update(req, res) {
+    const schema = Yup.object().shape({
+      name: Yup.string(),
+      email: Yup.string().email(),
+      oldPassword: Yup.string().min(6),
+      password: Yup.string()
+        .min(6)
+        .when('oldPassword', (oldPassword, field) =>
+          oldPassword ? field.required() : field
+        ), // Se a oldPassword for informada, a password deverá ser preenchida
+      confirmPassword: Yup.string().when('password', (password, field) =>
+        password ? field.required().oneOf([Yup.ref('password')]) : field
+      ), // Usando a password para comparar se as senhas batem
+    });
+
+    if (!(await schema.isValid(req.body))) {
+      return res.status(400).json({ error: 'Validation fails' });
+    }
+
     const { email, oldPassword } = req.body;
 
     // Buscando o usuário no banco de dados
