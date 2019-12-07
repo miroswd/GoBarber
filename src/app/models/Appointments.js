edfd335d99b5509cce055e8ee41d7679a52fda8c
miroswd <altamirsantos93@hotmail.com>
2019-12-06 22:57:44 -0300

    Date Validation

--- src/app/models/Appointments.js
+++ src/app/models/Appointments.js
@@ -15,10 +15,7 @@ class Appointments extends Model {
   static associate(models) {
     // Vai ser chamado automaticamente no loader de models (index.js)
     this.belongsTo(models.User, { foreignKey: 'user_id', as: 'user' });
-    this.belongsTo(models.User, {
-      foreignkey: 'provider_id',
-      as: 'provider',
-    });
+    this.belongsTo(models.User, { foreignKey: 'provider_id', as: 'provider' });
   }
 }
 export default Appointments;
