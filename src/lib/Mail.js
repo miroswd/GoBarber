220c08fdaa5a96ddab33652047461c27e1f95dd4
miroswd <altamirsantos93@hotmail.com>
2019-12-08 16:40:42 -0300

    Envio de Email

---
+++ src/lib/Mail.js
@@ -0,0 +1,22 @@
+// Configurações do envio de email
+import nodemailer from 'nodemailer';
+import mailConfig from '../config/mail';
+
+class Mail {
+  constructor() {
+    const { host, port, secure, auth } = mailConfig;
+
+    this.transporter = nodemailer.createTransport({
+      host,
+      port,
+      secure,
+      auth: auth.user ? auth : null,
+    }); // Como o nodemailer chama uma conexão externa para enviar email
+  }
+
+  sendMail(message) {
+    return this.transporter.sendMail({ ...mailConfig.default, ...message });
+  }
+}
+
+export default new Mail();
